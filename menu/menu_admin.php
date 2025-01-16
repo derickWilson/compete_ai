a3119dc61afc3fabb9b52afe40c2afbc51b93b58
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-01-16 15:27:07 -0300

    atualizado os menus

--- menu/menu_admin.php
+++ menu/menu_admin.php
@@ -3,11 +3,11 @@
         <a href="index.php">home</a>
         <a href="eventos.php">Eventos</a>
         <a href="../pagina_pessoal.php">Página pessoal</a>
-        <a href="painel_administrativo.php">Painel Administrativo</a>
+        <a href="admin/painel_administrativo.php">Painel Administrativo</a>
         <a href="eventos_cadastrados.php">Eventos Cadastrdos</a>
         <a href="admin/criar_eventos.php">Criar evento</a>
         <a href="admin/pessoas.php">Atletas</a>
-        <a href="../logout.php">Deslogar</a>
+        <a href="logout.php">Deslogar</a>
 
     </nav>
 </header>
