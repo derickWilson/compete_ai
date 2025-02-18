fae83dee222369dfd813b6fb041b47720979e2a3
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-02-18 14:32:34 -0300

    finalizando e estilizando

--- logar.php
+++ logar.php
@@ -1,27 +1,26 @@
 <?php
-try{
-require_once "classes/atletaService.php";
-require_once "classes/atletaClass.php";
-include "func/clearWord.php";
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    try{
+        require_once "classes/atletaService.php";
+        include "func/clearWord.php";
 
-} catch (Exception $e){
-    echo $e->getMessage();
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST"){    
+    }catch (Exception $e){
+        echo $e->getMessage();
+    }
     $atleta = new Atleta();
     $conn = new Conexao();
-    echo " conexao feita ";
-
+    
     $atleta->__set("email", cleanWords($_POST["usuario"]));
     $atleta->__set("senha", cleanWords($_POST["senha"]));
-    echo " <br> objetos criados";
 
     try {
         $attServ = new atletaService($conn, $atleta);
-        echo " <br> connex estab";
         $attServ->logar(); // this is where you call the logar method
-        header("Location: pagina_pessoal.php");
+        if(!$_SESSION["validado"]){
+            header('Location: login.php?erro=1');
+        }else{
+            header("Location: pagina_pessoal.php");   
+        }
     } catch (Exception $e) {
         echo "Erro ao tentar logar: " . $e->getMessage();
     }
