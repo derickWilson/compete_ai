59d767106272309a204ae82025f78876523edb61
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-01 13:25:45 -0300

    removido espaços de calcularIdade

--- func/calcularIdade.php
+++ func/calcularIdade.php
@@ -4,10 +4,8 @@ function calcularIdade($dataNascimento) {
     $nascimento = new DateTime($dataNascimento);
     // Cria um objeto DateTime para a data atual
     $hoje = new DateTime();
-    
     // Calcula a diferença entre os anos
     $idade = $hoje->format('Y') - $nascimento->format('Y');
-    
     // Retorna a idade
     return $idade;
 }
