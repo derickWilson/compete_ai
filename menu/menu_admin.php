bd2a7fea0fa428526a51a4ecf7a2632371d9113b
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-01 20:06:36 -0300

    menu admin corrigido

--- menu/menu_admin.php
+++ menu/menu_admin.php
@@ -1,12 +1,12 @@
 <header>
     <nav>
-        <a href="/compete_ai/index.php">Home</a>
-        <a href="/compete_ai/eventos.php">Eventos</a>
-        <a href="/compete_ai/pagina_pessoal.php">Página Pessoal</a>
-        <a href="/compete_ai/admin/painel_administrativo.php">Painel Administrativo</a>
-        <a href="/compete_ai/eventos_cadastrados.php">Eventos Cadastrados</a>
-        <a href="/compete_ai/admin/criar_eventos.php">Criar Evento</a>
-        <a href="/compete_ai/admin/pessoas.php">Atletas</a>
-        <a href="/compete_ai/logout.php">Deslogar</a>
+        <a href="/index.php">Home</a>
+        <a href="/eventos.php">Eventos</a>
+        <a href="/pagina_pessoal.php">Página Pessoal</a>
+        <a href="/admin/painel_administrativo.php">Painel Administrativo</a>
+        <a href="/eventos_cadastrados.php">Eventos Cadastrados</a>
+        <a href="/admin/criar_eventos.php">Criar Evento</a>
+        <a href="/admin/pessoas.php">Atletas</a>
+        <a href="/logout.php">Deslogar</a>
     </nav>
 </header>
