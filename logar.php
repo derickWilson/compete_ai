a58fa5dcf119b85a4eadd4cb36f1ea5a5c20df24
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-01 13:42:07 -0300

    espaços removidos

--- logar.php
+++ logar.php
@@ -3,23 +3,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     try{
         require_once "classes/atletaService.php";
         include "func/clearWord.php";
-
     }catch (Exception $e){
         echo $e->getMessage();
     }
     $atleta = new Atleta();
     $conn = new Conexao();
-    
     $atleta->__set("email", cleanWords($_POST["usuario"]));
     $atleta->__set("senha", cleanWords($_POST["senha"]));
-
     try {
         $attServ = new atletaService($conn, $atleta);
-        $attServ->logar(); // this is where you call the logar method
+        $attServ->logar(); 
     } catch (Exception $e) {
         echo "Erro ao tentar logar: " . $e->getMessage();
     }
-
 } else {
     header("Location: index.php");
 }
