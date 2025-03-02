c3facc5e629751b118201118a09657b63d152f67
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-02 10:56:25 -0300

    colocando estilo

--- index.php
+++ index.php
@@ -10,18 +10,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
     <?php
         include "menu/add_menu.php";
     ?>
     <h1>Bem Vindo ao fpjji</h1>
-    </header>
 <hr>
 <div>
     <?php
         if(isset($_GET["erro"]) && $_GET["erro"] == 2){
             // no caso do erro 2 mostra que a conta não foi validada
-            echo "<h3>Sua Conta Ainda Não Foi Validada</h3>";
+            echo "<h3 class='alert' >Sua Conta Ainda Não Foi Validada</h3>";
         }
     ?>
 </div>
