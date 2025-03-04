e0ed877328b4913eb95a4273608ebf5daa6933b0
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-04 20:43:33 -0300

    cadastrar atleta e filiar organizado

--- cadastro_academia.php
+++ cadastro_academia.php
@@ -13,37 +13,7 @@
     ?>
     <div class="principal">
     <form method="post" action="cadastrar.php" enctype="multipart/form-data">
-    Foto 3x4<br>
-    <input type="file" placeholder="FOTO" name="foto" id="foto" accept=".jpg,.jpeg,.png"><br>
-
-    Nome do Responsavel  <input name="nome" type="text" placeholder="nome completo" required><br>
-        <?php
-            if(isset($erro) && $erro == 1){
-                echo '<span class = "erro">usuario ja possui conta </span>';
-                echo 'email <input name="email" type="email" placeholder="exemplo@email.com" required><br>';
-            }else{
-                echo 'email <input name="email" type="email" placeholder="exemplo@email.com" required><br>';
-            }
-        ?>
-        senha  <input type="password" name="senha" id="senha" required><br>
-        Data de Nascimento  <input type="date" name="data_nascimento" id="data_nasc" required><br>
-        Fone  <input maxlength="12" type="tel" name="fone" id="telefone" placeholder="0000000000" required><br>
-        Nome da Academia/Equipe  <input type="text" name="academia" id="academia" required><br>
-        Faixa do Responsavel <select id="faixas" name="faixa" required>
-            <option value="">Graduação</option>
-            <option value="Branca">Branca</option>
-            <option value="Crinza">Crinza</option>
-            <option value="Amarela">Amarela</option>
-            <option value="Laranja">Laranja</option>
-            <option value="Verde">Verde</option>
-            <option value="Azul">Azul</option>
-            <option value="Roxa">Roxa</option>
-            <option value="Marrom">Marrom</option>
-            <option value="Preta">Preta</option>
-            <option value="Coral">Coral</option>
-            <option value="Vermelha e Branca">Vermelha e Branca</option>
-            <option value="Vermelha">Vermelha</option>
-        </select><br>
+    Nome da Academia/Equipe  <input type="text" name="academia" id="academia" required><br>
         CEP da academia<input maxlength="20" type="text" name="cep" id="cep" required><br>
         Cidade<input maxlength="50" type="text" name="cidade" id="cidade" required><br>
         Estado
@@ -75,6 +45,36 @@
           <option value="SP">São Paulo (SP)</option>
           <option value="SE">Sergipe (SE)</option>
           <option value="TO">Tocantins (TO)</option>
+        </select><br>
+    Foto 3x4<br>
+    <input type="file" placeholder="FOTO" name="foto" id="foto" accept=".jpg,.jpeg,.png"><br>
+
+    Nome do Responsavel  <input name="nome" type="text" placeholder="nome completo" required><br>
+        <?php
+            if(isset($erro) && $erro == 1){
+                echo '<span class = "erro">usuario ja possui conta </span>';
+                echo 'email <input name="email" type="email" placeholder="exemplo@email.com" required><br>';
+            }else{
+                echo 'email <input name="email" type="email" placeholder="exemplo@email.com" required><br>';
+            }
+        ?>
+        senha  <input type="password" name="senha" id="senha" required><br>
+        Data de Nascimento  <input type="date" name="data_nascimento" id="data_nasc" required><br>
+        Fone  <input maxlength="12" type="tel" name="fone" id="telefone" placeholder="0000000000" required><br>
+        Faixa do Responsavel <select id="faixas" name="faixa" required>
+            <option value="">Graduação</option>
+            <option value="Branca">Branca</option>
+            <option value="Crinza">Crinza</option>
+            <option value="Amarela">Amarela</option>
+            <option value="Laranja">Laranja</option>
+            <option value="Verde">Verde</option>
+            <option value="Azul">Azul</option>
+            <option value="Roxa">Roxa</option>
+            <option value="Marrom">Marrom</option>
+            <option value="Preta">Preta</option>
+            <option value="Coral">Coral</option>
+            <option value="Vermelha e Branca">Vermelha e Branca</option>
+            <option value="Vermelha">Vermelha</option>
         </select><br>
         Diploma<br>
         <input type="file" placeholder="DIPLOMA" name="diploma" id="diploma" accept=".jpg,.jpeg,.png"><br>
--- cadastro_atleta.php
+++ cadastro_atleta.php
@@ -29,11 +29,12 @@
         Data de Nascimento  <input type="date" name="data_nascimento" id="data_nasc" required><br>
         Fone  <input maxlength="12" type="tel" name="fone" id="telefone" placeholder="0000000000" required><br>
         Academia/Equipe  
-        <select id="faixas" name="faixa" required>
             <?php
             //pegas todas as academias filiadas
             ?>
-        Faixa <select id="faixas" name="faixa" required>
+        <br>
+        Faixa 
+        <select id="faixas" name="faixa" required>
             <option value="">Graduação</option>
             <option value="Branca">Branca</option>
             <option value="Crinza">Crinza</option>
