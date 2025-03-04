7b38ed95062496d1706ed91d90bfc255a18ff093
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-04 17:53:45 -0300

    fazendo cadastrar academia

--- cadastrar.php
+++ cadastrar.php
@@ -3,8 +3,33 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if($_POST["tipo"] == "A"){
         //cadastrar academia
-        $caminhoParaSalvar = null;
-        //Verifica se o arquivo foi enviado corretamente
+        //cadastrar academia primeiro
+        require_once "classes/atletaService.php";
+        include "func/clearWord.php";
+        $con = new Conexao();
+        $atletas = new Atleta();
+        $attServ = new atletaService($con, $atletas);
+        //filiar academia
+        $attServ->Filiar(cleanWords($_POST["academia"]),cleanWords($_POST["cep"]));
+        //Verifica se o diploma foi enviado corretamente
+        if (isset($_FILES['diploma']) && $_FILES['diploma']['error'] === UPLOAD_ERR_OK) {
+            $diploma = $_FILES['diploma'];
+            $extensao = pathinfo($diploma['name'], PATHINFO_EXTENSION);
+            $novoNome = 'diploma_' . time() . '.' . $extensao;
+            $caminhoParaSalvar = 'diplomas/' . $novoNome;
+            if ($diploma['size'] > 0) {
+                if (move_uploaded_file($diploma['tmp_name'], $caminhoParaSalvar)) {
+                } else {
+                    echo ' Erro ao mover arquivo. Verifique as permiss玫es do diret贸rio.';
+                    header("Location: cadastro.php");
+                    exit();
+                }
+            } else {
+                echo 'Arquivo vazio ou erro no upload';
+            }
+
+        }
+        //tratar foto enviada
         if (isset($_FILES['diploma']) && $_FILES['diploma']['error'] === UPLOAD_ERR_OK) {
             $diploma = $_FILES['diploma'];
             $extensao = pathinfo($diploma['name'], PATHINFO_EXTENSION);
@@ -21,11 +46,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo 'Arquivo vazio ou erro no upload';
             }
         }
-        require_once "classes/atletaService.php";
-        include "func/clearWord.php";
-        // Criptografe a senha
-        $con = new Conexao();
-        $atletas = new Atleta();
         $atletas->__set("nome", cleanWords($_POST["nome"]));
         $atletas->__set("senha", cleanWords($_POST["senha"]));
         $atletas->__set("email", cleanWords($_POST["email"]));
@@ -35,7 +55,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $atletas->__set("faixa", cleanWords($_POST["faixa"]));
         $atletas->__set("peso", $_POST["peso"]);
         $atletas->__set("diploma",$novoNome);
-        $attServ = new atletaService($con, $atletas);
         if($attServ->emailExists($_POST["email"])){
             header("Location: cadastro.php?erro=1");
             exit();
