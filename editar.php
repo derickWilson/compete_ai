58f81f8718159b1932ccdd50e5ce3c72a4d7d9ce
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-13 16:29:57 -0300

    corrigindo a edicao 4

--- editar.php
+++ editar.php
@@ -1,16 +1,8 @@
 <?php
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-
-    echo "<pre>";
-    print_r($_FILES);
-    echo "</pre>";
 // Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once "classes/atletaService.php";
     include "func/clearWord.php";
-    echo "to aqui.";
     // Criação do objeto de conexão e atleta
     $con = new Conexao();
     $at = new Atleta();
@@ -28,12 +20,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $caminhoParaSalvar = '/diplomas/' . $novoNome;
         //excluir antigo diploma
         if(!empty("/diplomas/".$diploma_antigo) && file_exists("/diplomas/".$diploma_antigo)){
-            unlink('diplomas/'.$diploma_antigo);
+            unlink('/diplomas/'.$diploma_antigo);
         }
         if ($diploma['size'] > 0) {
             if (move_uploaded_file($diploma['tmp_name'], $caminhoParaSalvar)) {
                 $diplomaNovo = $novoNome;
-                echo "troquei a diploma";
             } else {
                 echo 'Erro ao mover arquivo. Verifique as permissões do diretório.';
                 header("Location: editar_atleta.php");
@@ -50,15 +41,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $foto = $_FILES['foto_nova'];
         $extensaoFoto = pathinfo($foto['name'], PATHINFO_EXTENSION);
         $novoNomeFoto = 'foto_' . time() . '.' . $extensaoFoto;
-        $caminhoParaSalvarFoto = 'fotos/' . $novoNomeFoto;
+        $caminhoParaSalvarFoto = '/fotos/' . $novoNomeFoto;
         //excluir foto antiga   
-        if(!empty("/fotos/".$diploma_foto_antigaantigo) && file_exists("/fotos/".$foto_antiga)){
+        if(!empty('/fotos/'.$foto_antiga) && file_exists("/fotos/".$foto_antiga)){
             unlink('/fotos/'.$foto_antiga);
         }
         if ($foto['size'] > 0) {
             if (move_uploaded_file($foto['tmp_name'], $caminhoParaSalvarFoto)) {
                 $fotoNova = $novoNomeFoto;
-                echo "troquei a foto";
             } else {
                 echo ' Erro ao mover arquivo. Verifique as permiss玫es do diret贸rio.';
                 header("Location: cadastro_academia.php");
@@ -79,10 +69,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $at->__set("faixa", cleanWords($_POST["faixa"]));
     $at->__set("peso", cleanWords($_POST["peso"]));
     $at->__set("diploma", $diplomaNovo);
-
-    echo "<pre>";
-    print_r($at);
-    echo "</pre>";
     
     try {
         // Atualiza o atleta
