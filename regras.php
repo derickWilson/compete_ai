d5ed3b0396560c6e0beb923ed0589de01183132c
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-28 16:05:17 -0300

    regras adicionadas

---
+++ regras.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regras</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        include "menu/add_menu.php";
+    ?>
+<center><h3>Livro de regras</h3></center>
+<div>
+<object data="regras.pdf" type="application/pdf" width="100%" height="600px"></object>
+</div>
+<?php
+include "menu/footer.php";
+?>
+</body>
+</html>
