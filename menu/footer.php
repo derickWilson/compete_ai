90bf4f9e78b7eb9be4d9c1b4134fddfd8a501c51
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-28 16:37:42 -0300

    footer atualizado

--- menu/footer.php
+++ menu/footer.php
@@ -1,1 +1,8 @@
-<footer>todos os direitos reservados</footer>
+<footer>
+    FEDERAÇÃO PAULISTA DE JIU-JITSU INTERNACIONAL<br>
+    Cnpj 58.837.905/0001-09
+    E-mail <a href="mailto:fpjjioficial@gmail.com">fpjjioficial@gmail.com</a><br>
+    Contato  +55 16 99418-2926<br>
+    @fpjjioficial federação Paulista de jiu-jitsu internacional
+    <br>todos os direitos reservados
+</footer>
