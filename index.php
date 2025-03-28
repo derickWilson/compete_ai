721d13bc6649534c8421bdfe7c6ba6720ed79896
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-28 17:18:35 -0300

    estilo atualizado

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FPJJI</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="/estilos/icone.jpeg">
 </head>
 <body>
     <?php
