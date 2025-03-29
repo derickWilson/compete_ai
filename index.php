a69f654fdfd9d0bd2c14f8921baa21c76823e040
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-29 15:42:31 -0300

    adicionando patrocinador

--- index.php
+++ index.php
@@ -47,10 +47,10 @@
     </div>
     <?php }?>
 <center class="clear"><h3>Patrocinio</h3></center>
-    <div >
-        <a class="patrocinador" href=""><img src="patrocinio/patrocinador_1.jpeg"></a>
-        <a class="patrocinador" href=""><img src="patrocinio/patrocinador_2.jpeg"></a>
-        <a class="patrocinador" href=""><img src="patrocinio/patrocinador_3.jpeg"></a>
+    <div class="patrocinio-container">
+        <a class="patrocinador" href="https://lsisopradores.ind.br/"><img width="340px" height="140px" src="patrocinio/patrocinador_1.jpeg"></a>
+        <a class="patrocinador" href="https://multivix.edu.br/ead/"><img width="340px" height="140px" src="patrocinio/patrocinador_2.jpeg"></a>
+        <a class="patrocinador" href="https://www.instagram.com/lotususinagem_pecas"><img width="340px" height="140px" src="patrocinio/patrocinador_3.jpeg"></a>
     </div>
 <?php
 include "menu/footer.php";
