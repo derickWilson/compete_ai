8b1ee940162f0a664f53b20f22da98676d4c077a
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-03-31 19:51:49 -0300

    correcoes

--- menu/menu_admin.php
+++ menu/menu_admin.php
@@ -7,11 +7,11 @@
         <a href="/index.php">Home</a>
         <a href="/eventos.php">Eventos</a>
         <a href="/pagina_pessoal.php">Página Pessoal</a>
-        <a href="/admin/painel_administrativo.php">Painel Administrativo</a>
+        <a href="/admin/painel_administrativo.php">Painel Administrativo</a>    
         <a href="/eventos_cadastrados.php">Eventos Cadastrados</a>
         <a href="/admin/criar_eventos.php">Criar Evento</a>
         <a href="/admin/pessoas.php">Atletas</a>
         <a href="/logout.php">Deslogar</a>
-        <a href="regras.php">regras</a>
+        <a href="/regras.php">regras</a>
     </nav>
 </header>
