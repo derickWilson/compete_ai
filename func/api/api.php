99bfa80526fcd598c94e0c84c2f73e03624d52dc
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-04-17 17:56:02 -0300

    corrigido bd e tentado API

--- func/api/api.php
+++ func/api/api.php
@@ -22,4 +22,40 @@ $response = $client->request('POST', 'https://api-sandbox.asaas.com/v3/customers
 ]);
 
 echo $response->getBody();
+
+//criar cobrança
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api-sandbox.asaas.com/v3/payments",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode([
+    'billingType' => 'PIX',
+    'customer' => 'cus_1',
+    'value' => 10,
+    'externalReference' => 'campeonato_1',
+    'description' => 'campeonado: blablabla'
+  ]),
+  CURLOPT_HTTPHEADER => [
+    "accept: application/json",
+    "access_token: toke_BLABLABLA",
+    "content-type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 ?>
