568f8f5052dcdc94705bb05e13c5e7d800ad771b
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-04-29 15:53:47 -0300

    terminado API testando

--- admin/baixar.php
+++ admin/baixar.php
@@ -26,10 +26,10 @@ if (isset($_GET["id"])) {
 
     // Cabeçalho do arquivo CSV
     $header = array(
-        "Inscrição","Campeonato", "Atleta", "Idade", "Faixa",
-        "Peso", "Academia", "modalidade","Com Quimono", "Sem Quimono", "Absoluto Com Quimono",
-        "Absoluto Sem Quimono"
-    );
+        "Inscrição", "Campeonato", "Atleta", "Idade", "Faixa",
+        "Peso", "Academia", "Modalidade", "Com Quimono", "Sem Quimono", 
+        "Absoluto Com Quimono", "Absoluto Sem Quimono", "Status Pagamento", "Valor Pago"
+    );    
 
     // Nome do arquivo CSV
     $filename = "inscritos_evento_{$id}.csv";
@@ -43,28 +43,29 @@ if (isset($_GET["id"])) {
     // Escreve o cabeçalho no arquivo CSV
     fputcsv($output, $header);
     // Escreve os dados no arquivo CSV
-    foreach ($dados as $value) {
-        fputcsv($output, [
-            $value->id.$value->ide,
-            $value->evento,
-            $value->inscrito,
-            calcularIdade($value->data_nascimento),
-            $value->faixa,
-            $value->peso,
-            $value->academia,
-            $value->modalidade,
-            $value->mod_com ? "X" : "",
-            $value->mod_sem ? "X" : "",
-            $value->mod_ab_com ? "X" : "",
-            $value->mod_ab_sem ? "X" : ""
-        ]);
-    }
+    fputcsv($output, [
+        $value->id . $value->ide,
+        $value->evento,
+        $value->inscrito,
+        calcularIdade($value->data_nascimento),
+        $value->faixa,
+        $value->peso,
+        $value->academia,
+        $value->modalidade,
+        $value->mod_com ? "X" : "",
+        $value->mod_sem ? "X" : "",
+        $value->mod_ab_com ? "X" : "",
+        $value->mod_ab_sem ? "X" : "",
+        $value->status_pagamento === 'RECEIVED' ? "Pago" : "Pendente",
+        $value->valor_pago ? number_format($value->valor_pago, 2, ',', '.') : "0,00"
+    ]);
+    
 
     // Fecha o fluxo de saída
     fclose($output);
     exit();
 } else {
     echo "Erro";
-    header("Location /admin/eventos.php");
+    header("Location: /admin/eventos.php");
 }
 ?>
