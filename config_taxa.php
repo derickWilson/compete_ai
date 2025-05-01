a17e2b21b4626d170a02b3aadc7ad05e9b4e1b05
Derick Wilson Pereira do Nascimento <derickwpn@usp.br>
2025-05-01 20:22:15 -0300

    melhorando estilo

---
+++ config_taxa.php
@@ -0,0 +1,5 @@
+<?php
+// config_taxa.php
+define('TAXA_PORCENTAGEM', 1.0); // Taxa em porcentagem
+define('taxa', 1 + (TAXA_PORCENTAGEM / 100)); // Converte porcentagem para fator multiplicador
+?>
